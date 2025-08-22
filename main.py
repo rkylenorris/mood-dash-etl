7e4f91183cec1e3e5075e63c946010fc5d982a71
Kyle Norris <r.kyle.norris@gmail.com>
2025-08-22 11:30:36 -0400

    created functional approach for getting and extracting data from daylio

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def main():
     print("Hello from daylio-data-cleaner!")
+    from log_setup import logger
+    from extractor.daylio_extractor import get_latest_backup
+    print(get_latest_backup())
 
 
 if __name__ == "__main__":
