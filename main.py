2c73af3a65c89da334a79a9c7f4400293972fd9d
Kyle Norris <r.kyle.norris@gmail.com>
2025-08-29 14:23:22 -0400

    setup for testing fitbit sleep module

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from log_setup import logger
 from extractor.data_extractor import extract_daylio_data
+from fitbit_sleep import clean_sleep_data
 
 
 def main():
     logger.info("Hello from daylio-data-cleaner!")
-    extract_daylio_data()
+    # extract_daylio_data()
+    sleep_data = clean_sleep_data()
+    print(sleep_data.head())
 
 
 if __name__ == "__main__":
