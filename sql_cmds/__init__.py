20433eef2a47c305d451ad0f5c344f80737bca5c
Kyle Norris <r.kyle.norris@gmail.com>
2025-09-01 15:17:03 -0400

    created add_users function for adding users on initial db build

--- sql_cmds/__init__.py
+++ sql_cmds/__init__.py
@@ -1,2 +1,3 @@
 from .db_init import create_tables, create_views, insert_prefs
 from .sql_cmds import create_db_conn, read_sql_view_to_df, execute_sql_command, execute_sql_script
+from .add_users import add_users
--- sql_cmds/add_users.py
+++ sql_cmds/add_users.py
@@ -1,39 +1,52 @@
-from sql_cmds import create_db_conn, execute_sql_command
-from enum import Enum
+import os
 import bcrypt
+
+from enum import Enum
+from dotenv import load_dotenv
+from sql_cmds import create_db_conn, execute_sql_command
+
+load_dotenv()
+
 
 class UserRole(Enum):
     ADMIN = 'admin'
     USER = 'user'
     PROVIDER = 'provider'
 
+
 def add_user(username: str, name: str, password: str, role: UserRole, db_path: str = "data/daylio.db"):
-    
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-    
+
+    hashed_password = bcrypt.hashpw(password.encode(
+        'utf-8'), bcrypt.gensalt()).decode('utf-8')
+
     sql_cmd = "INSERT INTO users (username, name, password_hash, role) VALUES (?, ?, ?, ?)"
     user_data = (username, name, hashed_password, role.value)
-    
+
     execute_sql_command(create_db_conn(db_path), sql_cmd, True, user_data)
 
 # TODO: Run script to add users, create login page, and set up authentication
-if __name__ == "__main__":
+
+
+def add_users():
     logins = [
         {
             "username": "admin",
             "name": "Kyle Norris",
-            "password": "admin123",
+            "password": os.getenv('ADMIN_PASSWORD', None),
             "role": UserRole.ADMIN
         },
         {
             "username": "therapist",
             "name": "Amanda Cosby",
-            "password": "provider123",
+            "password": os.getenv('PROVIDER_PASSWORD', None),
             "role": UserRole.PROVIDER
         }
     ]
-    
+
     for login in logins:
+        if login["password"] is None:
+            raise ValueError(
+                f"Password for user {login['username']} not set in environment variables.")
         add_user(
             username=login["username"],
             name=login["name"],
