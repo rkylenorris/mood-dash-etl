8abd31aca87e7b7779a48ebb89e42f71fa68bc7b
Kyle Norris <r.kyle.norris@gmail.com>
2025-09-01 15:36:22 -0400

    changed db path to point to .env file

--- sql_cmds/db_init.py
+++ sql_cmds/db_init.py
@@ -1,16 +1,21 @@
-from datetime import datetime
-from log_setup import logger
+import os
 
-from .sql_cmds import create_db_conn, execute_sql_script, Path, execute_sql_command
+from log_setup import logger
+from datetime import datetime
+from dotenv import load_dotenv
+
 from .calendar_cmds import create_rolling_calendar
 
-home_dir = Path(__file__).parent.parent
+
+from .sql_cmds import create_db_conn, execute_sql_script, Path, execute_sql_command
+load_dotenv()
+
+home_dir = Path().cwd()
 data_dir = home_dir / "data"
-db_path = data_dir / "daylio.db"
+db_path = os.getenv('DB_PATH', home_dir / "mood_dash.db")
 sql_dir = home_dir / "sql"
 create_tables_script = sql_dir / "create_tables.sql"
 create_views_script = sql_dir / "create_views.sql"
-
 
 
 def create_tables(db_conn=create_db_conn(str(db_path))):
@@ -20,33 +25,40 @@ def create_tables(db_conn=create_db_conn(str(db_path))):
     logger.info("Creating rolling calendar to-date and loading into sql db")
     rolling_calendar = create_rolling_calendar()
 
-    rolling_calendar.to_sql('calendar', db_conn, if_exists="replace", index=False)
-    
+    rolling_calendar.to_sql('calendar', db_conn,
+                            if_exists="replace", index=False)
+
     db_conn.commit()
     db_conn.close()
-    
+
+
 def create_views(db_conn=create_db_conn(str(db_path))):
     logger.info("Executing script to create requisite views for data charting")
     execute_sql_script(db_conn, str(create_views_script))
     db_conn.commit()
     db_conn.close()
-    
+
+
 def insert_prefs(prefs_dict, db_conn=create_db_conn(str(db_path))):
     insert_query = '''
     INSERT INTO prefs 
     (AUTO_BACKUP_IS_ON, LAST_DAYS_IN_ROWS_NUMBER, DAYS_IN_ROW_LONGEST_CHAIN, LAST_ENTRY_CREATION_TIME) 
     VALUES (?, ?, ?, ?)
     '''
-    bckup = next(filter(lambda x: x['key'] == 'AUTO_BACKUP_IS_ON', prefs_dict))['value']
-    last_days_inarow = next(filter(lambda x: x['key'] == 'LAST_DAYS_IN_ROWS_NUMBER', prefs_dict))['value']
-    longest_days_inarow = next(filter(lambda x: x['key'] == 'DAYS_IN_ROW_LONGEST_CHAIN', prefs_dict))['value']
-    last_entry_time = next(filter(lambda x: x['key'] == 'LAST_ENTRY_CREATION_TIME', prefs_dict))['value']
+    bckup = next(filter(lambda x: x['key'] ==
+                 'AUTO_BACKUP_IS_ON', prefs_dict))['value']
+    last_days_inarow = next(
+        filter(lambda x: x['key'] == 'LAST_DAYS_IN_ROWS_NUMBER', prefs_dict))['value']
+    longest_days_inarow = next(
+        filter(lambda x: x['key'] == 'DAYS_IN_ROW_LONGEST_CHAIN', prefs_dict))['value']
+    last_entry_time = next(
+        filter(lambda x: x['key'] == 'LAST_ENTRY_CREATION_TIME', prefs_dict))['value']
     last_entry_time = datetime.fromtimestamp(last_entry_time/1000)
-    
+
     vals = [bckup, last_days_inarow, longest_days_inarow, last_entry_time]
-    
+
     logger.info("Creating and inserting 'prefs' table and values")
-    
+
     execute_sql_command(db_conn, insert_query, True, vals)
     db_conn.commit()
     db_conn.close()
